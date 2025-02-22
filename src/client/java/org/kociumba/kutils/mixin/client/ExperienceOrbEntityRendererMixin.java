60e0a20d69cac183bc6290376af2116323d8647e
kociumba <79542688+kociumba@users.noreply.github.com>
2025-02-22 15:22:27 +0100

    move the check for enabling custom xp orbs before the size and opacity checks

--- src/client/java/org/kociumba/kutils/mixin/client/ExperienceOrbEntityRendererMixin.java
+++ src/client/java/org/kociumba/kutils/mixin/client/ExperienceOrbEntityRendererMixin.java
@@ -36,12 +36,12 @@ public abstract class ExperienceOrbEntityRendererMixin extends EntityRenderer {
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void onRender(ExperienceOrbEntity experienceOrbEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light, CallbackInfo ci) {
-        if (getC().getCustomXpOrbSize() == 0.0f && getC().getCustomXpOrbColor().getAlpha() == 0) {
-            ci.cancel(); // skip rendering 0 size and 0 alpha orbs to improve performance
-        }
-
         if (!getC().getShouldUsecustomXpOrbs()) {
             return;
+        }
+
+        if (getC().getCustomXpOrbSize() == 0.0f && getC().getCustomXpOrbColor().getAlpha() == 0) {
+            ci.cancel(); // skip rendering 0 size and 0 alpha orbs to improve performance
         }
 
         matrixStack.push();
